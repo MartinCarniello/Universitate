2955b167d7ddfefb4e50c38742083d346f9af9b9
Martin Carniello <mcarniello@tsjbaires.gov.ar>
2016-09-13 14:28:41 -0300

    Chat functionality Send messages in conversation with another user via ajax (Still remains sockets integration).

--- universitate/app/assets/javascripts/conversations.js
+++ universitate/app/assets/javascripts/conversations.js
@@ -1,14 +1,30 @@
-// $(function(){
-//   $(".chat").niceScroll();
-// })
-
-function ready() {
+function scrollBottom() {
   var chat = $(".chat-table-scrollable");
   chat.removeClass('hidden');
   if(chat.length > 0)
     chat.scrollTop(chat[0].scrollHeight);
 }
 
-$(document).ready(ready);
+function submitMessage() {
+  $("#message_message").unbind("keypress").bind({
+    keypress: function(event) {
+      if(event.which == 13) {
+        $("#new_message").submit();
+      }
+    }
+  });
 
+  $("span.answer-btn").unbind("click").bind({
+    click: function() {
+      $("#new_message").submit();
+    }
+  });
+}
+
+function ready() {
+  scrollBottom();
+  submitMessage();
+}
+
+$(document).ready(ready);
 $(document).on('turbolinks:load', ready)
--- universitate/app/controllers/conversations_controller.rb
+++ universitate/app/controllers/conversations_controller.rb
@@ -5,6 +5,17 @@ class ConversationsController < ApplicationController
 
   def show
     @conversation = Conversation.find(params[:id])
+    @message = Message.new(conversation: @conversation, sender: current_user, receiver: @conversation.decorate.linked_user)
     @messages = @conversation.messages
+  end
+
+  def update
+    @message = Message.create(message_required_params)
+  end
+
+  private
+
+  def message_required_params
+    params.require(:message).permit(:sender_id, :receiver_id, :conversation_id, :message)
   end
 end
