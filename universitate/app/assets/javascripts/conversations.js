c6f20ab16f04024d925aa686966891876643fae6
Martin Carniello <mcarniello@tsjbaires.gov.ar>
2016-09-14 14:50:54 -0300

    Encapsulate js files in functions

--- universitate/app/assets/javascripts/conversations.js
+++ universitate/app/assets/javascripts/conversations.js
@@ -1,29 +1,31 @@
-function scrollBottom() {
-  var chat = $(".chat-table-scrollable");
-  chat.removeClass('hidden');
-  if(chat.length > 0)
-    chat.scrollTop(chat[0].scrollHeight);
-}
+(function() {
+  function scrollBottom() {
+    var chat = $(".chat-table-scrollable");
+    chat.removeClass('hidden');
+    if(chat.length > 0)
+      chat.scrollTop(chat[0].scrollHeight);
+  }
 
-function submitMessage() {
-  $("#message_message").unbind("keypress").bind({
-    keypress: function(event) {
-      if(event.which == 13) {
+  function submitMessage() {
+    $("#message_message").unbind("keypress").bind({
+      keypress: function(event) {
+        if(event.which == 13) {
+          $("#new_message").submit();
+        }
+      }
+    });
+
+    $("span.answer-btn").unbind("click").bind({
+      click: function() {
         $("#new_message").submit();
       }
-    }
-  });
+    });
+  }
 
-  $("span.answer-btn").unbind("click").bind({
-    click: function() {
-      $("#new_message").submit();
-    }
-  });
-}
+  function ready() {
+    scrollBottom();
+    submitMessage();
+  }
 
-function ready() {
-  scrollBottom();
-  submitMessage();
-}
-
-$(document).ready(ready);
+  $(document).ready(ready);
+})();
