7bbf709d0be4535f02d291dbce62feb680b54e5d
Martin Carniello <mcarniello@tsjbaires.gov.ar>
2016-09-14 10:13:19 -0300

    Change path of empty-user image in uploader.

--- universitate/app/uploaders/avatar_uploader.rb
+++ universitate/app/uploaders/avatar_uploader.rb
@@ -21,7 +21,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   
-    "/assets/empty_user.png"
+    "/empty_user.png"
   end
 
   # Process files as they are uploaded:
