b474a50baee90d823fddc3481866de7afc74b8ae
Mateo <mate.qac@gmail.com>
2016-09-23 20:31:40 -0300

    Fixed upload image for profile

--- universitate/app/controllers/teachers_controller.rb
+++ universitate/app/controllers/teachers_controller.rb
@@ -17,11 +17,12 @@ class TeachersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    uploader = AvatarUploader.new
+
 
     if @user.update(user_params)
-      @user.avatar= params[:avatar]
+      uploader.store!(params[:avatar])
       flash[:notice] = I18n.t('views.teacher_profile.edit.updated_successfuly')
-      @user.save!
       redirect_to root_path
     else
       render :show
@@ -51,7 +52,6 @@ class TeachersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name,  :avatar, :location_attributes => [:lat, :lng, :full_address], :teacher_profile_attributes => [:description, :id, :hour_rate, :subject_ids => [], :works_attributes =>[:name_of_the_place, :period_start, :period_end, :description, :id, :_destroy], :studies_attributes =>[:name_of_the_place, :period_start, :period_end, :description, :id, :_destroy]])
-
+    params.require(:user).permit(:first_name, :last_name,  :avatar, :avatar_cache, :location_attributes => [:lat, :lng, :full_address], :teacher_profile_attributes => [:description, :id, :hour_rate, :subject_ids => [], :works_attributes =>[:name_of_the_place, :period_start, :period_end, :description, :id, :_destroy], :studies_attributes =>[:name_of_the_place, :period_start, :period_end, :description, :id, :_destroy]])
   end
 end
