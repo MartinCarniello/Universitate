ad7754da8975784aaea0b98f0973c80acafe3691
Mateo <mate.qac@gmail.com>
2016-09-27 14:57:57 -0300

    Email layout modified

--- universitate/app/controllers/ratings_controller.rb
+++ universitate/app/controllers/ratings_controller.rb
@@ -1,6 +1,8 @@
 class RatingsController < ApplicationController
   def create
     @rating = Rating.create(rating_required_params)
+    @user = TeacherProfile.find(@rating.teacher_profile_id).user
+    #UserMailer.rating_email(@user).deliver
   end
 
   def rating_required_params
