87894b114dcaf9ab93d7ead0fed07d3cae29129b
Martin Carniello <martincarniello@gmail.com>
2016-10-03 17:55:33 -0300

    Add sweet alert 2 plugin for ratings and replies notifications

--- universitate/app/controllers/ratings_controller.rb
+++ universitate/app/controllers/ratings_controller.rb
@@ -3,11 +3,13 @@ class RatingsController < ApplicationController
     @rating = Rating.create(rating_required_params)
     @user = TeacherProfile.find(@rating.teacher_profile_id).user
     UserMailer.rating_email(@user).deliver
+    flash.now[:notice] = I18n.t('views.teacher_profile.rating.create_success')
   end
 
   def update
     @rating = Rating.find(params[:rating][:id])
     @rating.update_attributes(rating_required_params)
+    flash.now[:notice] = I18n.t('views.teacher_profile.rating.update_success')
   end
 
   private
