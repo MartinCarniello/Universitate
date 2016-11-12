28779986865a3d7aa7fb147d28c696270bd10656
Mateo <mate.qac@gmail.com>
2016-11-12 10:02:30 -0300

    Adding admin page

--- universitate/db/schema.rb
+++ universitate/db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104172737) do
+ActiveRecord::Schema.define(version: 20161111163252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "conversations", force: :cascade do |t|
     t.integer  "first_user_id"
